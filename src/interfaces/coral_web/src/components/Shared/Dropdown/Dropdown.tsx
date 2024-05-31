4a650bec145279933a00d0e0a840f5626d63482e
Salman Khan <salmankhan@xmos.com>
2024-05-31 14:53:16 +0100

    fix dropdown component changing to controlled from uncontrolled

--- src/interfaces/coral_web/src/components/Shared/Dropdown/Dropdown.tsx
+++ src/interfaces/coral_web/src/components/Shared/Dropdown/Dropdown.tsx
@@ -117,152 +117,156 @@ export const Dropdown: React.FC<Props> = ({
       {kind === 'default' && label && (
         <InputLabel className="mb-2" label={label} tooltipLabel={tooltipLabel} />
       )}
-      <Listbox
-        value={value}
-        onChange={onChangeListBox}
-        disabled={disabled}
-        as="div"
-        className="relative"
-      >
-        {({ open }) => (
-          <div
-            className={cn(
-              roundedClasses,
-              borderClasses,
-              'border-marble-500 bg-marble-100 hover:bg-marble-200',
-              {
-                'rounded-b-lg': !open,
-                'rounded-bl-none rounded-br-none': open,
-              }
-            )}
-          >
-            <Listbox.Button
+      {
+        value &&
+        <Listbox
+          value={value}
+          onChange={onChangeListBox}
+          disabled={disabled}
+          as="div"
+          className="relative testing"
+        >
+          {({ open }) => (
+            <div
               className={cn(
-                'relative flex w-full items-center justify-between disabled:cursor-not-allowed',
-                'rounded-lg focus:outline-none focus-visible:outline-1 focus-visible:outline-offset-4 focus-visible:outline-volcanic-900',
+                roundedClasses,
+                borderClasses,
+                'border-marble-500 bg-marble-100 hover:bg-marble-200',
                 {
-                  'px-3 py-4': kind === 'default' || !label,
-                  'px-3 pb-2.5 pt-7': kind === 'cell' && label,
-                },
-                buttonClassName
+                  'rounded-b-lg': !open,
+                  'rounded-bl-none rounded-br-none': open,
+                }
               )}
-              data-testid={dataTestId}
             >
-              {kind === 'cell' && label && (
-                <InputLabel
-                  className="absolute left-0 top-0 w-fit pl-3 pt-2.5"
-                  label={label}
-                  tooltipLabel={tooltipLabel}
-                />
-              )}
-              <Text
-                as="div"
-                className={cn('mr-2 w-full truncate text-left', {
-                  'text-volcanic-800': value === undefined,
-                })}
-              >
-                {valueLabel ?? placeholder}{' '}
-                {valueDescription && (
-                  <Text as="span" className="text-volcanic-600">
-                    ({valueDescription})
-                  </Text>
+              <Listbox.Button
+                className={cn(
+                  'relative flex w-full items-center justify-between disabled:cursor-not-allowed',
+                  'rounded-lg focus:outline-none focus-visible:outline-1 focus-visible:outline-offset-4 focus-visible:outline-volcanic-900',
+                  {
+                    'px-3 py-4': kind === 'default' || !label,
+                    'px-3 pb-2.5 pt-7': kind === 'cell' && label,
+                  },
+                  buttonClassName
                 )}
-              </Text>
-              <Icon name="chevron-down" />
-            </Listbox.Button>
-
-            <Listbox.Options
-              className={cn(
-                'absolute left-0',
-                'max-h-80 w-full overflow-y-auto bg-marble-100',
-                'rounded-b-lg border border-marble-500',
-                'z-dropdown',
-                'focus:outline-none focus-visible:outline-1 focus-visible:outline-offset-4 focus-visible:outline-volcanic-900'
-              )}
-            >
-              {searchable && (
-                <div className="sticky top-0 border-b border-marble-500 bg-white">
-                  <section className="p-3">
-                    <Input
-                      placeholder="filter model"
-                      className="m-0"
-                      size="sm"
-                      kind="default"
-                      onChange={(e) => setSearchTerm(e.target.value)}
-                    />
-                  </section>
-
-                  {options.length === 0 && searchTerm !== '' && (
-                    <>
-                      <hr className="border-marble-500" />
-                      <Text className="p-4">No results found</Text>
-                    </>
-                  )}
-                </div>
-              )}
-
-              {options.map((group, i) => (
-                <Fragment key={i}>
-                  {group.title && (
-                    <Text
-                      key={group.title}
-                      styleAs="overline"
-                      className={cn('mx-3 py-3 font-medium', {
-                        'border-b border-marble-500':
-                          i !== optionGroups.length - 1 || group.options.length > 0,
-                      })}
-                    >
-                      {group.title}
+                data-testid={dataTestId}
+              >
+                {kind === 'cell' && label && (
+                  <InputLabel
+                    className="absolute left-0 top-0 w-fit pl-3 pt-2.5"
+                    label={label}
+                    tooltipLabel={tooltipLabel}
+                  />
+                )}
+                <Text
+                  as="div"
+                  className={cn('mr-2 w-full truncate text-left', {
+                    'text-volcanic-800': value === undefined,
+                  })}
+                >
+                  {valueLabel ?? placeholder}{' '}
+                  {valueDescription && (
+                    <Text as="span" className="text-volcanic-600">
+                      ({valueDescription})
                     </Text>
                   )}
-                  {group.options
-                    .filter((option) => {
-                      if (!searchable || !searchTerm) {
-                        return true;
-                      }
-                      return (
-                        option.label?.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                        option.value?.toLowerCase().includes(searchTerm.toLowerCase())
-                      );
-                    })
-                    .map((option, j) => (
-                      <Listbox.Option
-                        key={option.value}
-                        value={option.value}
-                        className={({ active }) =>
-                          cn('cursor-pointer px-3', {
-                            'bg-green-50': active,
-                            'rounded-b-lg':
-                              i === optionGroups.length - 1 && j === group.options.length - 1,
-                          })
-                        }
-                        data-testid={`dropdown-option-${option.value}`}
+                </Text>
+                <Icon name="chevron-down" />
+              </Listbox.Button>
+  
+              <Listbox.Options
+                className={cn(
+                  'absolute left-0',
+                  'max-h-80 w-full overflow-y-auto bg-marble-100',
+                  'rounded-b-lg border border-marble-500',
+                  'z-dropdown',
+                  'focus:outline-none focus-visible:outline-1 focus-visible:outline-offset-4 focus-visible:outline-volcanic-900'
+                )}
+              >
+                {searchable && (
+                  <div className="sticky top-0 border-b border-marble-500 bg-white">
+                    <section className="p-3">
+                      <Input
+                        placeholder="filter model"
+                        className="m-0"
+                        size="sm"
+                        kind="default"
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                      />
+                    </section>
+  
+                    {options.length === 0 && searchTerm !== '' && (
+                      <>
+                        <hr className="border-marble-500" />
+                        <Text className="p-4">No results found</Text>
+                      </>
+                    )}
+                  </div>
+                )}
+  
+                {options.map((group, i) => (
+                  <Fragment key={i}>
+                    {group.title && (
+                      <Text
+                        key={group.title}
+                        styleAs="overline"
+                        className={cn('mx-3 py-3 font-medium', {
+                          'border-b border-marble-500':
+                            i !== optionGroups.length - 1 || group.options.length > 0,
+                        })}
                       >
-                        {({ selected }) => (
-                          <Text
-                            as="div"
-                            className={cn('break-words py-3 text-volcanic-800', {
-                              'font-medium': selected,
-                              'border-b border-marble-500':
-                                i !== optionGroups.length - 1 || j !== group.options.length - 1,
-                            })}
-                          >
-                            {option.label ?? option.value}{' '}
-                            {option.description && (
-                              <Text as="span" className="text-volcanic-600">
-                                ({option.description})
-                              </Text>
-                            )}
-                          </Text>
-                        )}
-                      </Listbox.Option>
-                    ))}
-                </Fragment>
-              ))}
-            </Listbox.Options>
-          </div>
-        )}
-      </Listbox>
+                        {group.title}
+                      </Text>
+                    )}
+                    
+                    {group.options
+                      .filter((option) => {
+                        if (!searchable || !searchTerm) {
+                          return true;
+                        }
+                        return (
+                          option.label?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                          option.value?.toLowerCase().includes(searchTerm.toLowerCase())
+                        );
+                      })
+                      .map((option, j) => (
+                        <Listbox.Option
+                          key={option.value}
+                          value={option.value}
+                          className={({ active }) =>
+                            cn('cursor-pointer px-3', {
+                              'bg-green-50': active,
+                              'rounded-b-lg':
+                                i === optionGroups.length - 1 && j === group.options.length - 1,
+                            })
+                          }
+                          data-testid={`dropdown-option-${option.value}`}
+                        >
+                          {({ selected }) => (
+                            <Text
+                              as="div"
+                              className={cn('break-words py-3 text-volcanic-800', {
+                                'font-medium': selected,
+                                'border-b border-marble-500':
+                                  i !== optionGroups.length - 1 || j !== group.options.length - 1,
+                              })}
+                            >
+                              {option.label ?? option.value}{' '}
+                              {option.description && (
+                                <Text as="span" className="text-volcanic-600">
+                                  ({option.description})
+                                </Text>
+                              )}
+                            </Text>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                  </Fragment>
+                ))}
+              </Listbox.Options>
+            </div>
+          )}
+        </Listbox>
+      }
       {description && (
         <Text as="span" styleAs="caption" className="mt-2 text-volcanic-700">
           {description}
